e676e36ea76e4acb6f2d661a4381b13190140456
Nicholas O'Connor <43360323+Earth-Turtle@users.noreply.github.com>
2025-10-16 21:21:37 -0700

    adapt complexity to gdscript

--- complexity_calculator.gd
+++ complexity_calculator.gd
@@ -1,95 +1,106 @@
 class_name ComplexityCalculator
-extends RefCounted
+extends Node
 
-static func complexity_analysis(path: String) -> float:
-	var lzv_arr = lzv(path)
-	var lzv_encoded_length = lzv_arr[0]
-	var lzv_dict_size = lzv_arr[1]
-	var entropy = string_entropy(path)
+enum Direction {RIGHT, UP, LEFT, DOWN}
+
+const directions = ["R", "U", "L", "D"]
+const e = 2.718281828459045235360287471352
+const compressability_factor = 0.5
+const no_repeats_factor = 0.8
+const entropy_factor = 0.5
+
+static func complexity_analysis_dir_arr(path: Array[Direction]) -> float:
+	var converted = ""
+	for dir in path:
+		match dir:
+			Direction.RIGHT: converted += "R"
+			Direction.UP: converted += "U"
+			Direction.LEFT: converted += "L"
+			Direction.DOWN: converted += "D"
+	return complexity_analysis(converted)
 	
-	var complexity = (1.0 * lzv_encoded_length) * (1.0 * lzv_dict_size) * (1.0 * (entropy + 1)) * (1.0 * path.length())
-	return complexity
+	
+## The full, headache-inducing complexity function. Takes in a string consisting of only R, U, L, and D characters
+## Returns a float rounded to two decimal places
+static func complexity_analysis(path: String) -> float:
+	return snappedf(
+		base_length_multiplier(path) * (
+			(((compressability_multiplier(path) - 1) * compressability_factor) + 1)
+			* (((no_repeats_bonus(path) - 1) * no_repeats_factor) + 1)
+			* (1 + (pattern_entropy(path) * entropy_factor / 2))
+		) ** 1.3
+	, 0.01)
 
 ## Calculates the Shannon entropy of a given string
-static func string_entropy(input: String) -> float:
-	var counts = {
-		"R": input.countn("R"),
-		"U": input.countn("U"),
-		"L": input.countn("L"),
-		"D": input.countn("D"),
-	}
-	
+static func pattern_entropy(input: String) -> float:	
 	var entropy = 0.0
-	for character in counts:
-		var char_count = counts[character]
+	for dir in directions:
+		var char_count = input.countn(dir)
 		if char_count == 0: continue
 		var char_probability = float(char_count) / input.length()
-		entropy += char_probability * log(char_probability) / log(2)
-	return -entropy
+		entropy -= char_probability * log(char_probability) / log(2)
+	return entropy
 	
 ## Adaptation of Lempel-Ziv-Welch encoding to determine the "compressability" of a string
 ## Returns a 2-element array: the length of the output encoded string, and the length of the dictionary
-static func lzv(input: String) -> Array[int]:
-	var dict = {}
-	const possible_dirs = ["L", "U", "R", "D"]
-	for dir in possible_dirs:
+static func lempel_ziv(input: String) -> Array[int]:
+	var encoding_dict = {}
+	for dir in directions:
 		if input.contains(dir):
-			dict[dir] = dict.size()
+			encoding_dict[dir] = str(encoding_dict.size())
 	
 	var working_string = ""
 	var output_codes = []
 	for c in input:
-		if dict.has(working_string + c):
+		if encoding_dict.has(working_string + c):
 			working_string += c
 		else:
-			output_codes.append(dict[working_string])
-			dict[working_string + c] = str(dict.size())
+			output_codes.append(encoding_dict[working_string])
+			encoding_dict[working_string + c] = str(encoding_dict.size())
 			working_string = c
 	
-	output_codes.append(dict[working_string])
+	output_codes.append(encoding_dict[working_string])
+	return [output_codes.size(), encoding_dict.size()]
 	
-	return [output_codes.size(), dict.size()]
-	
-const test_patterns = [
-	"R",
-	"U",
-	"L",
-	"RR",
-	"RU",
-	"LL",
-	"UD",
-	"RRR",
-	"URL",
-	"RLD",
-	"RLR",
-	"UDU",
-	"RDULUL",
-	"RLRLR",
-	"RLRLRL",
-	"RLRL",
-	"URDLURDL",
-	"ULRDULRD",
-	"RLUDRL",
-	"RLUDRR",
-	"RRLU",
-	"LURR",
-	"LRRU",
-	"RDULRUDLLUDR",
-	"LDURLDLDURDLDLLR",
-	"DDDDRRRRLUUUUU",
-	"URURURURURURURURURURUR",
-	"RRRR",
-	"RRRRR",
-	"RRRRRR",
-	"RRRRRRR",
-	"RRRRRRRR",
-	"RRRRRRRRR",
-	"RRRRRRRRRR",
-	"RRRRRRRRRRR",
-]
+static func base_length_multiplier(input: String) -> float:
+	return (input.length() - 1) * 0.1 + 1	
 
-static func test():
-	var complexities = test_patterns.map(func (p): return [p, complexity_analysis(p)])
-	complexities.sort_custom(func (a, b): return a[1] > b[1])
-	for entry in complexities:
-		print(entry)
+static func compressability_multiplier(input: String) -> float:
+	var frac = input.length() / (1.0 + input.length() - lempel_ziv(input)[0])
+	return log(9 + max(1, frac)) / log(10)
+	
+#region repeated letters bonus
+
+static func count_repeated_letters(input: String) -> int:
+	if input.length() == 0 or input.length() == 1: return 0
+	var count = 0
+	for i in range(input.length() - 1):
+		if input[i] == input[i+1]: count += 1
+	return count
+
+## The lowest the bonus could be (asymptotically). 1 is good, can be lower to reduce the chance that any bonus is given for many repeats
+const lower_bound = 0.97
+## How fast the maximum bonus scales based on length. This is an exponent, should keep this between 1 and 2, leaning 1
+const max_bonus_scaling = 1.3
+## At what length should the no-repeats bonus be 2
+const length_for_double = 18.0
+## From 0 to 1, where 1 is the whole string as one repeated input, where half of the bonus should be received.
+## E.g., 0.2 means half of the max possible bonus is received when 20% of the string has repeated inputs
+const proportion_for_half_bonus = 0.2
+## Steepness of the curve. Higher values make the curve steeper, and flatten the asymptotic approach on either side of the midpoint.
+## Must be greater than 0
+const steepness = 1.25
+## Additional parameter for controlling character of the curve. > 0
+const skew = 1.5
+
+static func no_repeats_bonus(input: String) -> float:
+	var upper_bound = 1.0 + ((input.length() / length_for_double) ** max_bonus_scaling)
+	# intermediate variable so it doesn't get too hairy
+	var mid_shift_inside_ln = (((upper_bound - lower_bound) ** skew) / ((((upper_bound + lower_bound) / 2.0) - lower_bound) ** skew)) - 1
+	var mid_shift = input.length() * proportion_for_half_bonus - (1.0 / steepness) * log(mid_shift_inside_ln)
+	
+	var exponent = steepness * (count_repeated_letters(input) - mid_shift)
+	var bottom_of_frac = (1 + e ** exponent) ** (1 / skew)
+	return max(1, lower_bound + ((upper_bound - lower_bound) / bottom_of_frac))
+
+#endregion
